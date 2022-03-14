116dc6abb6c3ddd149b54d6012f95aa210da0ad6
Rodriguez316 <100016420+Rodriguez316@users.noreply.github.com>
2022-03-14 13:38:26 -0600

    Add files via upload
    Lab Exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,32 @@
+def simple():
+    print(value)
+    print(value2)
+    print(value3)
+    print(value4)
+    print(value5)
+
+
+value = 1
+value2 = 2
+value3 = 3
+value4 = 4
+value5 = 5
+
+print(value,value2,value3,value4,value5)
+
+def simple2():
+    print(value6)
+
+value6 = 6
+
+print(value6)
+value6 = 'six'
+print(value6)
+value6 = '06'
+print(value6)
+value6 = 'seis'
+print(value6)
+
+def favorite_food(food):
+    print(f"favorite food is: {food}")
+favorite_food("tacos")
