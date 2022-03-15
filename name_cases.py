2ddd005e4be9fb0e379c0a092b3e92b88e5719b3
Rodriguez316 <100016420+Rodriguez316@users.noreply.github.com>
2022-03-15 07:36:07 -0600

    Add files via upload

---
+++ name_cases.py
@@ -0,0 +1,20 @@
+first_name = "Chris"
+last_name = "Rodriguez"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, it is a good day to learn Python {full_name}!")
+
+print(full_name.upper())
+print(full_name.lower())
+print(f"Hello, {full_name.title()}!")
+
+print("Ice Cube once said, \"Today was a good day!\"")
+
+famous_person = "Ice Cube"
+print(famous_person + " once said, \"Today was a good day!\"")
+
+name = "    Babe Ruth   "
+name = "    \t Babe Ruth\n  "
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+print(name)
