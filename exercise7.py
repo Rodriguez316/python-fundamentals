cffbe04d58762b768eb3d7de16a283a156c9008b
Rodriguez316 <100016420+Rodriguez316@users.noreply.github.com>
2022-03-16 08:21:31 -0600

    Add files via upload

---
+++ exercise7.py
@@ -0,0 +1,29 @@
+# 4-3
+
+for value in range(1,21):
+    print(value)
+
+#4-4
+
+for value in range(1,1000000):
+    print(value)
+
+
+#4-6
+
+for value in range(1, 21,2):
+    print(value)
+
+#4-7
+
+mult_of_three = []
+for value in range(3,30,3):
+    print(value)
+
+# 4-8
+cube_list = [value**3 for value in range(1,10)]
+for value in cube_list:
+    print(value)
+
+
+
