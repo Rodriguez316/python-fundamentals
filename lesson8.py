1c3d0420a57b2482b7990c6e733ace6cd48011f5
Rodriguez316 <100016420+Rodriguez316@users.noreply.github.com>
2022-03-16 10:34:32 -0600

    Add files via upload

---
+++ lesson8.py
@@ -0,0 +1,37 @@
+# Lesson 8 Collections
+
+# list collection
+# ordered collection that can be changeable and allow duplicates
+# and uses square brackets to define the collected data
+
+colors = ['red', 'blue', 'green', 'purple', 'magenta', 'black',
+          'white', 'orange']
+sizes = [42, 32, 156, 89, 34, 23, 8, 19]
+
+mixed = [23, 'mixed', 'python', 90, 45, 'snow', 'car', 56]
+
+
+# direct print
+def simple_print():
+    print(colors)
+
+# simple_print()
+
+
+# print 1 item
+def simple_single():
+    print(sizes[3])
+
+# simple_single()
+
+
+
+
+# for loop of list
+def simple_for_print():
+    for mix in mixed:
+        print(mix)
+
+
+simple_for_print()
+
